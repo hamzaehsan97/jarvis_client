760a451002b3a37a1c4c2b2a2ee1b07649c184bb
hamzaehsan97 <hamzaehsan75@gmail.com>
2022-12-31 19:55:23 -0800

    get notes

--- src/views/sample-page/index.js
+++ src/views/sample-page/index.js
@@ -5,64 +5,75 @@ import { Typography } from '@mui/material';
 import MainCard from 'ui-component/cards/MainCard';
 import { DataGrid } from '@mui/x-data-grid';
 import Box from '@mui/material/Box';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const columns = [
-    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'id', headerName: 'ID', width: 30 },
     {
-        field: 'firstName',
-        headerName: 'First name',
+        field: 'content',
+        headerName: 'Content',
+        width: 600,
+        editable: true
+    },
+    {
+        field: 'type',
+        headerName: 'Type',
+        type: 'text',
+        width: 80,
+        editable: true
+    },
+    {
+        field: 'creationTime',
+        headerName: 'Time',
         width: 150,
         editable: true
-    },
-    {
-        field: 'lastName',
-        headerName: 'Last name',
-        width: 150,
-        editable: true
-    },
-    {
-        field: 'age',
-        headerName: 'Age',
-        type: 'number',
-        width: 110,
-        editable: true
-    },
-    {
-        field: 'fullName',
-        headerName: 'Full name',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: false,
-        width: 160,
-        valueGetter: (params) => `${params.row.firstName || ''} ${params.row.lastName || ''}`
     }
 ];
 
-const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 }
-];
+const SamplePage = () => {
+    const [rows, setRows] = useState([{ id: 0 }]);
+    const token = localStorage.getItem('token');
+    const config = {
+        headers: {
+            token: token
+        }
+    };
 
-const SamplePage = () => (
-    <MainCard title="Notes">
-        <Box sx={{ height: 400, width: '100%' }}>
-            <DataGrid
-                rows={rows}
-                columns={columns}
-                pageSize={5}
-                rowsPerPageOptions={[5]}
-                checkboxSelection
-                disableSelectionOnClick
-                experimentalFeatures={{ newEditingApi: true }}
-            />
-        </Box>
-    </MainCard>
-);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(() => {
+        async function fetchData() {
+            axios
+                .get('https://jarvis-backend-test.herokuapp.com/texties', config)
+                .then((result) => {
+                    let collectRows = [];
+                    result.data.forEach(function (val, index) {
+                        collectRows.push({ id: index, ...val });
+                    });
+                    setRows(collectRows);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <MainCard title="Notes">
+            <Box sx={{ height: 400, width: '100%' }}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    pageSize={5}
+                    rowsPerPageOptions={[5]}
+                    checkboxSelection
+                    disableSelectionOnClick
+                    experimentalFeatures={{ newEditingApi: true }}
+                />
+            </Box>
+        </MainCard>
+    );
+};
 
 export default SamplePage;

783df2adffc9502347d4f63fcbb1148f732ac509
Hamza Ehsan <hamzaehsan75@gmail.com>
2023-11-29 04:23:10 +0000

    Add links for privacy policy and terms of service

--- src/ui-component/cards/AuthFooter.js
+++ src/ui-component/cards/AuthFooter.js
@@ -1,8 +1,23 @@
 // material-ui
-import { Link, Typography, Stack } from '@mui/material';
+import { Typography, Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+import PrivacyPolicy from 'views/pages/PrivacyPolicy';
 
 // ==============================|| FOOTER - AUTHENTICATION 2 & 3 ||============================== //
 
-const AuthFooter = () => <Stack direction="row" justifyContent="space-between"></Stack>;
+const AuthFooter = () => (
+    <Stack direction="row" spacing={2}>
+        <span>
+            <Typography component={Link} to="/privacy-policy" variant="subtitle1">
+                Privacy Policy
+            </Typography>
+        </span>
+        <span>
+            <Typography component={Link} to="/terms-of-service" variant="subtitle1">
+                Terms of Service
+            </Typography>
+        </span>
+    </Stack>
+);
 
 export default AuthFooter;

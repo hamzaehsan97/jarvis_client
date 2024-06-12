150fca8365bab414ec235d575cde86a2432209d4
Hamza Ehsan <hamzaehsan75@gmail.com>
2024-06-12 10:20:48 -0700

    Update AuthRegister.js

--- src/views/pages/authentication/auth-forms/AuthRegister.js
+++ src/views/pages/authentication/auth-forms/AuthRegister.js
@@ -122,7 +122,7 @@ const FirebaseRegister = ({ ...others }) => {
                     });
                     axios
                         .post(
-                            'http://jarvisloadbalancer-800577279.us-west-2.elb.amazonaws.com:8080/users?email=' +
+                            'https://jarvisloadbalancer-800577279.us-west-2.elb.amazonaws.com:8080/users?email=' +
                                 values.email +
                                 '&password=' +
                                 values.password +
